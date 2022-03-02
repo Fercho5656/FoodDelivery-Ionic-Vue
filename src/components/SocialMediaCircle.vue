39656121d30d0bab32dda585df759243a60c0c6f
Fercho5656 <morenomoranfernando@gmail.com>
2022-03-02 08:16:21 -0600

    Circle with slot for social media ion-icon

---
+++ src/components/SocialMediaCircle.vue
@@ -0,0 +1,37 @@
+<template>
+  <a :href="url" target="_blank" rel="noopener nonreferer">
+    <span>
+      <slot></slot>
+    </span>
+  </a>
+</template>
+
+<script setup>
+defineProps({
+  url: String,
+  color: String,
+});
+</script>
+
+<style scoped>
+span {
+  width: 75px;
+  height: 75px;
+
+  border: 5px solid #ccc;
+  border-radius: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+a:hover span {
+  background-color: v-bind(color);
+}
+</style>
