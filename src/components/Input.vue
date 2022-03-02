98bd61b9498ee4146c7c4b1f3e86552eb6924192
Fercho5656 <morenomoranfernando@gmail.com>
2022-03-02 08:15:40 -0600

    Input component with slot for ion-icon

---
+++ src/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-item>
+    <ion-label position="floating"> <slot></slot>{{ placeholder }} </ion-label>
+    <ion-input
+      :type="type"
+      :value="modelValue"
+      :required="required"
+      @input="$emit('update:modelValue', $event.target.value)"
+    ></ion-input>
+  </ion-item>
+</template>
+
+<script setup>
+import { IonItem, IonLabel, IonInput } from "@ionic/vue";
+defineProps({
+  type: {
+    type: String,
+    default: "text",
+  },
+  placeholder: String,
+  modelValue: String,
+  required: Boolean,
+});
+</script>
+
+<style scoped>
+slot {
+  font-size: 5em;
+}
+</style>
