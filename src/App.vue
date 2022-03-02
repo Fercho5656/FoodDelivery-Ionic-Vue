8dcc98ff49eed7ed6969cce4204179643a13b2ad
Fercho5656 <morenomoranfernando@gmail.com>
2022-03-02 11:03:09 -0600

    Added roboto font

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,9 @@ export default {
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
