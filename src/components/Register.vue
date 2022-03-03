e303e1e7811af58a8b350fcaaeea4c26fa2ede75
Fercho5656 <morenomoranfernando@gmail.com>
2022-03-03 17:34:04 -0600

    Register SFC for Auth View

---
+++ src/components/Register.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-header>
+    <ion-text color="dark">
+      <h1>Create an account</h1>
+    </ion-text>
+  </ion-header>
+  <ion-card>
+    <ion-card-header>
+      <ion-icon :icon="personCircle"></ion-icon>
+    </ion-card-header>
+  </ion-card>
+</template>
+
+<script script>
+import {
+  IonHeader,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonText,
+  IonIcon,
+} from "@ionic/vue";
+
+import {personCircle} from "ionicons/icons";
+import Input from "./Input.vue";
+</script>
+
+<style>
+</style>
