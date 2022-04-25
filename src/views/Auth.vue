ff8262a7d346c555043801fa9cf3e4c5110581cf
Fercho5656 <morenomoranfernando@gmail.com>
2022-04-25 02:18:09 -0500

    Changed from ref to shallowRef

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -7,9 +7,9 @@
 import Footer from "../components/Footer.vue";
 import Login from "../components/Login.vue";
 import Register from "../components/Register.vue";
-import { ref } from "vue";
+import { ref, shallowRef } from "vue";
 
-const component = ref(Login);
+const component = shallowRef(Login);
 
 const switchViews = (newView) => {
   component.value = newView;
