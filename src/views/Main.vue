2fe2875faa2a3012c79c2db3d1b358b7104838cd
Fercho5656 <morenomoranfernando@gmail.com>
2022-05-26 14:56:47 -0500

    App main view

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,33 @@
 <template>
-    <h1>Food :D</h1>
+    <ion-page>
+        <ion-content>
+            <h1>Food :D</h1>
+            <ul>
+                <li v-for="i in 10">
+                    <FoodCard imgSrc='https://i.imgur.com/JTAn4e0.jpg' :stars="i" :timeToCook="i" :price="i" />
+                </li>
+            </ul>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script setup>
+import {
+    IonPage,
+    IonContent
+} from '@ionic/vue'
+
+import FoodCard from '../components/FoodCard.vue';
 </script>
 
 <style scoped>
+ul {
+    list-style: none;
+    list-style-position: inside;
+    padding-left: 0;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-gap: 20px;
+}
 </style>
