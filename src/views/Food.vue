979a8f33214960b0f7a6bc50d7ab7e1ba540aed6
Fercho5656 <morenomoranfernando@gmail.com>
2022-05-27 01:33:46 -0500

    Component ready * Modal to show form to add form * Button to open modal * Fetch existent food

--- src/views/Food.vue
+++ src/views/Food.vue
@@ -5,13 +5,20 @@
                 <ion-button slot="start">
                     <ion-back-button default-href="/"></ion-back-button>
                 </ion-button>
+                <ion-button slot="end" @click="addFoodModal = true">
+                    <ion-icon :icon="addOutline"></ion-icon>
+                </ion-button>
             </ion-toolbar>
         </ion-header>
         <ion-content>
+            <Modal :show="addFoodModal" @close="addFoodModal = false">
+                <AddFood @send-food="onSend"/>
+            </Modal>
             <h1>Food :D</h1>
             <ul>
-                <li v-for="i in 10">
-                    <FoodCard imgSrc='https://i.imgur.com/JTAn4e0.jpg' :stars="i" :timeToCook="i" :price="i" />
+                <li v-for="food in foods">
+                    <FoodCard :imgSrc="food.imageUrl" :imgAlt="food.description" :stars="food.stars" :price="food.price"
+                        :name="food.name" :timeToCook="food.preparationMinutes" />
                 </li>
             </ul>
         </ion-content>
@@ -24,11 +31,32 @@ import {
     IonContent,
     IonToolbar,
     IonHeader,
+    IonIcon,
     IonButton,
     IonBackButton
 } from '@ionic/vue'
 
+import { addOutline } from 'ionicons/icons';
+
 import FoodCard from '../components/FoodCard.vue';
+import AddFood from '../components/AddFood.vue';
+import Modal from '../components/Modal.vue';
+import { onMounted, ref } from 'vue'
+
+import { getAllFood } from '../services/food'
+
+const foods = ref();
+const addFoodModal = ref(false);
+
+onMounted(async () => {
+    const data = await getAllFood();
+    foods.value = data;
+})
+
+const onSend = (food) => {
+    foods.value.push(food);
+    addFoodModal.value = false;
+}
 </script>
 
 <style scoped>
